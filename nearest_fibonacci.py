1b5cb3cfe7ca0104b759ab6d1ba4234881b91a4c
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-09-21 19:28:11 +0530

    n=int(input()) a=0 b=1 x=0 y=0 while(n!=0):     if(a<n):         x=a     if(a>n):         y=a         break     c=a+b     a=b     b=c if(n-x<y-n):     print(x) elif(n-x==y-n):     print(x,y) else:     print(y)

---
+++ nearest_fibonacci.py
@@ -0,0 +1,20 @@
+n=int(input())
+a=0
+b=1
+x=0
+y=0
+while(n!=0):
+    if(a<n):
+        x=a
+    if(a>n):
+        y=a
+        break
+    c=a+b
+    a=b
+    b=c
+if(n-x<y-n):
+    print(x)
+elif(n-x==y-n):
+    print(x,y)
+else:
+    print(y)
