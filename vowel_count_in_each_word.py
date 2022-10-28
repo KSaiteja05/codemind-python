399b1acc2c3a75f0ba894a504e572d681932f3b1
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-10-28 20:48:09 +0530

    n=list(map(str,input().split())) l=['a','e','i','o','u'] m=0 cnt=0 for i in range(len(n)):     s=n[i]     for j in range(len(s)):         if(s[j] in l):             cnt+=1     print(cnt,end=' ')     cnt=0

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,11 @@
+n=list(map(str,input().split()))
+l=['a','e','i','o','u']
+m=0
+cnt=0
+for i in range(len(n)):
+    s=n[i]
+    for j in range(len(s)):
+        if(s[j] in l):
+            cnt+=1
+    print(cnt,end=' ')
+    cnt=0
