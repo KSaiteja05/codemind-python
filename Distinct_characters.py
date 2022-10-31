9bf808da7bac95a6a535ed9f86cf0503d228fd3f
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-10-31 20:47:37 +0530

    n=input() n=n.lower() s=set(n) x=list(s) x.sort() for i in range(len(x)):     if(x[i]!=' '):         print(x[i],end='')

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,8 @@
 n=input()
 n=n.lower()
-l=[]
-for i in range(len(n)):
-    if(n.count(n[i])==1 and n[i]!=' '):
-        l.append(n[i])
-l.sort()
-for i in range(len(l)):
-    print(l[i],end='')
+s=set(n)
+x=list(s)
+x.sort()
+for i in range(len(x)):
+    if(x[i]!=' '):
+        print(x[i],end='')
