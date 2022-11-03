cf4912cc7d2769cd34da969d8668024083e6ec6b
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-11-03 19:35:50 +0530

    n=input() sq=int(n)*int(n) if(n in str(sq)):     print("Automorphic Number") else:     print("Not an Automorphic Number")

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,13 +1,6 @@
-m=int(input())
-x=len(str(m))
-s=0
-n=m*m
-while(x>0):
-    r=n%10
-    s=s*10+r
-    n=n//10
-    x-=1
-if(m==int(str(s)[::-1])):
-    print('Automorphic Number')
+n=input()
+sq=int(n)*int(n)
+if(n in str(sq)):
+    print("Automorphic Number")
 else:
-    print('Not an Automorphic Number')
+    print("Not an Automorphic Number")
