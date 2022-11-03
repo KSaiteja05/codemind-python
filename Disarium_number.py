858afc3669e3902148506b28c7b895407b97edfc
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-11-03 11:03:00 +0530

    n=input() s=0 j=1 for i in range(len(n)):     s+=int(n[i])**j     j+=1 print(s==int(n))

--- Disarium_number.py
+++ Disarium_number.py
@@ -1,15 +1,7 @@
-n=int(input())
-def isdis(n):
-    i=int(len(str(n)))
-    x=n
-    s=0
-    while(n>0):
-        r=n%10
-        s=s+r**i
-        n=n//10
-        i-=1
-    if(s==x):
-        return 1
-    else:
-        return 0
-print(isdis(n)==1)
+n=input()
+s=0
+j=1
+for i in range(len(n)):
+    s+=int(n[i])**j
+    j+=1
+print(s==int(n))
