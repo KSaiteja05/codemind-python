0597eef6fa90375cabc7dd05eaf803adee591176
sai teja Kodi <102404293+KSaiteja05@users.noreply.github.com>
2022-11-03 19:31:39 +0530

    n=int(input()) def isprime(n):     if(n<=1):         return 0     for i in range(2,n):         if(n%i==0):             return 0     return 1 if(isprime(n)==1 and isprime(int(str(n)[::-1]))==1):     print("circular prime") elif(isprime(n)==1):     print("prime but not a circular prime") else:     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,16 +1,14 @@
-def isprime(n):
-    cnt=0
-    for i in range(1,n+1):
-        if(n%i==0):
-            cnt+=1
-    if(cnt==2):
-        return True
-    else:
-        return False
 n=int(input())
+def isprime(n):
+    if(n<=1):
+        return 0
+    for i in range(2,n):
+        if(n%i==0):
+            return 0
+    return 1
 if(isprime(n)==1 and isprime(int(str(n)[::-1]))==1):
     print("circular prime")
 elif(isprime(n)==1):
     print("prime but not a circular prime")
 else:
-    print('not prime')
+    print("not prime")
